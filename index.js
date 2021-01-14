16ab5aa245a28d8ae65a56126fa6a42c53c97370
june-remind101 <june-remind101@users.noreply.github.com>
2021-01-14 12:41:32 -0800

    checking if we can filter down simple whoms

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function getOctokitClient() {
+  const token = core.getInput("GITHUB_TOKEN", { required: true });
+  return github.getOctokit(token);
+}
+
+async function getWhoms(client) {
+  const whoms = core.getInput("WHOMS", { required: true}).split(',').map((m) => m.trim()).map((m) => {
+    if (m.indexOf("@") === 0) {
+      client.info(`Should look up ${m}`);
+      return m
+    }
+    return m;
+  });
+
+  client.info(`whoms: ${JSON.stringify(whoms)}`);
+}
+
+async function main() {
+  const client = getOctokitClient();
+  getWhoms(client);
+}
+
+main();
